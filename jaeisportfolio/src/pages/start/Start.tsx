2d5ed3cce1882e747eddf710f3070af1695c9627
JaeiKim <dobbykim0320@gmail.com>
2024-01-17 05:25:12 +0900

    [Design] startPage 디자인 (세부효과 작업 전)

--- jaeisportfolio/src/pages/start/Start.tsx
+++ jaeisportfolio/src/pages/start/Start.tsx
@@ -2,6 +2,9 @@
 import { useNavigate } from "react-router-dom";
 //Component
 import { Backbody } from "../../components/common/box/body";
+//Assests
+import startPage from "../../assests/img/copy.png";
+import { Container } from "../../components/common/box/Container";
 
 function Start() {
   const navigate = useNavigate();
@@ -9,8 +12,19 @@ function Start() {
     navigate("/main");
   };
   return (
-    <Backbody onClick={goMain} style={{ cursor: "pointer" }}>
-      <h1>초기페이지</h1>
+    <Backbody onClick={goMain}>
+      <Container
+        style={{
+          backgroundImage: `url(${startPage})`,
+          backgroundSize: "cover", // 이미지를 가득 채우도록 설정
+          backgroundRepeat: "no-repeat", // 반복 없음
+          backgroundPosition: "center", // 중앙에 배치
+          width: "100%",
+          height: "100%",
+          cursor: "pointer",
+        }}
+        onClick={goMain}
+      />
     </Backbody>
   );
 }
